ee7b5cbc125d6d7e5243264b5468b1089e5c3065
yat-ming <49545797+yat-ming@users.noreply.github.com>
2020-01-31 17:08:21 +0800

    Update 第五周.c

--- 第五周.c
+++ 第五周.c
@@ -7,6 +7,7 @@ Write your code in this editor and press "Run" button to compile and execute it.
 *******************************************************************************/
 //阶乘
 //n!=1*2*3*...*n
+//1.for
 #include <stdio.h>
 
 int main()
@@ -20,3 +21,37 @@ int main()
     printf("%d!=%d",d,sum);
     return 0;
 }
+//2.while
+#include <stdio.h>
+
+int main()
+{
+    int d;
+    int i=1;
+    int sum=1;
+    scanf("%d",&d);
+   while(i<=d){
+       sum*=i;
+       i++;
+   }
+   printf("%d!=%d",d,sum);
+    return 0;
+}
+
+//奇偶数
+int main()
+{
+   int n;
+   scanf("%d",&n);
+   for(int i=1;i<=n;i++){
+       if(i%2==0){
+           printf("%d为偶数 ",i);
+       }
+       else{
+           printf("%d为奇数 ",i);
+       }
+   }
+    return 0;
+}
+
+//素数
